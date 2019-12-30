486a43e155e20afb2705d7b7c27e21766f12814b
J. Ritchie Carroll <ritchiecarroll@gmail.com>
2019-12-30 17:44:27 -0500

    Added more model based expression tests

--- src/UnitTests/ModelTests.cs
+++ src/UnitTests/ModelTests.cs
@@ -92,9 +92,11 @@ namespace Gemstone.Expressions.UnitTests
     {
         private static readonly Func<SimpleModel> s_createSimpleModel;
         private static readonly Action<SimpleModel> s_applyDefaultsToSimpleModel;
+        private static readonly Action<SimpleModel> s_applyUpdatesToSimpleModel;
 
         private static readonly Func<AdvancedModelContext, AdvancedModel> s_createAdvancedModel;
         private static readonly Action<AdvancedModelContext> s_applyDefaultsToAdvancedModel;
+        private static readonly Action<AdvancedModelContext> s_applyUpdatesToAdvancedModel;
 
         static ModelTests()
         {
@@ -103,9 +105,11 @@ namespace Gemstone.Expressions.UnitTests
 
             s_createSimpleModel = ValueExpressionParser<SimpleModel>.CreateInstance();
             s_applyDefaultsToSimpleModel = ValueExpressionParser<SimpleModel>.ApplyDefaults();
+            s_applyUpdatesToSimpleModel = ValueExpressionParser<SimpleModel>.UpdateInstance();
 
             s_createAdvancedModel = ValueExpressionParser<AdvancedModel>.CreateInstance<AdvancedModelContext>();
             s_applyDefaultsToAdvancedModel = ValueExpressionParser<AdvancedModel>.ApplyDefaults<AdvancedModelContext>();
+            s_applyUpdatesToAdvancedModel = ValueExpressionParser<AdvancedModel>.UpdateInstance<AdvancedModelContext>();
         }
 
         [TestMethod]
@@ -115,16 +119,36 @@ namespace Gemstone.Expressions.UnitTests
             SimpleModel simple1 = s_createSimpleModel();
 
             Assert.IsTrue(simple1.ID != Guid.Empty);
+            Assert.AreEqual(simple1.DoubleVal, Defaults.DoubleVal);
+            Assert.AreEqual(simple1.IntVal, Defaults.IntVal);
+            Assert.AreEqual(simple1.LongVal, Defaults.LongVal);
+            Assert.AreEqual(simple1.BoolVal, Defaults.BoolVal);
             Assert.AreEqual(simple1.CreatedBy, Defaults.CurrentUser);
             Assert.AreEqual(simple1.UpdatedBy, simple1.CreatedBy);
+            Assert.IsTrue(simple1.CreatedOn.Day == DateTime.UtcNow.Day);
+            Assert.AreEqual(simple1.CreatedOn, simple1.UpdatedOn);
 
             // Apply attribute defaults to an existing model
             SimpleModel simple2 = new SimpleModel();
             s_applyDefaultsToSimpleModel(simple2);
 
+            Assert.IsTrue(simple2.ID != Guid.Empty);
+            Assert.AreEqual(simple2.DoubleVal, Defaults.DoubleVal);
+            Assert.AreEqual(simple2.IntVal, Defaults.IntVal);
+            Assert.AreEqual(simple2.LongVal, Defaults.LongVal);
+            Assert.AreEqual(simple2.BoolVal, Defaults.BoolVal);
             Assert.AreEqual(simple2.CreatedBy, simple1.CreatedBy);
             Assert.AreEqual(simple2.UpdatedBy, simple2.CreatedBy);
-            Assert.IsTrue((simple2.CreatedOn - simple1.CreatedOn).TotalSeconds < 1.0D);
+            Assert.IsTrue((simple2.CreatedOn - simple1.CreatedOn).TotalSeconds < 10.0D);
+            Assert.AreEqual(simple2.CreatedOn, simple2.UpdatedOn);
+
+            // Apply updates to existing model, i.e., UpdateValueExpression attributes
+            s_applyUpdatesToSimpleModel(simple1);
+            
+            Assert.AreEqual(simple1.UpdatedBy, Defaults.CurrentUser);
+            Assert.AreEqual(simple1.UpdatedBy, simple1.CreatedBy);
+            Assert.IsTrue(simple1.UpdatedOn.Day == DateTime.UtcNow.Day);
+            Assert.IsTrue(simple1.UpdatedOn > simple1.CreatedOn);
         }
 
         [TestMethod]
@@ -136,20 +160,40 @@ namespace Gemstone.Expressions.UnitTests
             AdvancedModel advanced1 = s_createAdvancedModel(context);
 
             Assert.IsTrue(advanced1.ID != Guid.Empty);
+            Assert.AreEqual(advanced1.DoubleVal, Defaults.DoubleVal);
+            Assert.AreEqual(advanced1.IntVal, Defaults.IntVal);
+            Assert.AreEqual(advanced1.LongVal, Defaults.LongVal);
+            Assert.AreEqual(advanced1.BoolVal, Defaults.BoolVal);
             Assert.AreEqual(advanced1.CreatedBy, Defaults.CurrentUser);
             Assert.AreEqual(advanced1.UpdatedBy, advanced1.CreatedBy);
+            Assert.IsTrue(advanced1.CreatedOn.Day == DateTime.UtcNow.Day);
+            Assert.AreEqual(advanced1.CreatedOn, advanced1.UpdatedOn);
             Assert.IsTrue(advanced1.Ticks > 0L);
 
             // Apply attribute defaults to an existing model
             AdvancedModel advanced2 = new AdvancedModel();
-
             context.Instance = advanced2;
             s_applyDefaultsToAdvancedModel(context);
 
+            Assert.IsTrue(advanced2.ID != Guid.Empty);
+            Assert.AreEqual(advanced2.DoubleVal, Defaults.DoubleVal);
+            Assert.AreEqual(advanced2.IntVal, Defaults.IntVal);
+            Assert.AreEqual(advanced2.LongVal, Defaults.LongVal);
+            Assert.AreEqual(advanced2.BoolVal, Defaults.BoolVal);
             Assert.AreEqual(advanced2.CreatedBy, advanced1.CreatedBy);
             Assert.AreEqual(advanced2.UpdatedBy, advanced2.CreatedBy);
-            Assert.IsTrue((advanced2.CreatedOn - advanced1.CreatedOn).TotalSeconds < 1.0D);
+            Assert.IsTrue((advanced2.CreatedOn - advanced1.CreatedOn).TotalSeconds < 10.0D);
+            Assert.AreEqual(advanced2.CreatedOn, advanced2.UpdatedOn);
             Assert.IsTrue(advanced2.Ticks > 0L);
+
+            // Apply updates to existing model, i.e., UpdateValueExpression attributes
+            context.Instance = advanced1;
+            s_applyUpdatesToAdvancedModel(context);
+
+            Assert.AreEqual(advanced1.UpdatedBy, Defaults.CurrentUser);
+            Assert.AreEqual(advanced1.UpdatedBy, advanced1.CreatedBy);
+            Assert.IsTrue(advanced1.UpdatedOn.Day == DateTime.UtcNow.Day);
+            Assert.IsTrue(advanced1.UpdatedOn > advanced1.CreatedOn);
         }
     }
 }
